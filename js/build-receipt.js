7aaacb3beec77273aa9d37c4be86332f0d5b40f5
Ben <b.cylin@gmail.com>
2012-11-20 17:40:07 +0800

    Update build profiles

--- js/build-receipt.js
+++ js/build-receipt.js
@@ -1,15 +1,17 @@
 ({
-	baseUrl: './',
+	// node r.js -o baseUrl=. name=receipt.js out=receipt-built.js paths.jquery=lib/jquery-1.7.2.min ...
+	baseUrl: '.',
 	name: 'receipt.js',
 	out: 'receipt-built.js',
 	paths: {
-		jquery : 'lib/jquery-1.7.2.min',
-		order : 'lib/order',
-		underscore : 'lib/underscore.min',
-		backbone : 'lib/backbone.min',
-		scrollTo : 'plugin/jquery.scrollTo.min',
+		'jquery' : 'lib/jquery-1.7.2.min',
+		'underscore' : 'lib/underscore.min',
+		'backbone' : 'lib/backbone.min',
+		'scrollTo' : 'plugin/jquery.scrollTo.min',
 		'bootstrap-modal' : 'plugin/bootstrap-modal',
 		'bootstrap-tooltip' : 'plugin/bootstrap-tooltip',
 		'bootstrap-transition' : 'plugin/bootstrap-transition'
-	}
+	},
+	// the file where to find the shim config
+	mainConfigFile: 'receipt.js'
 })
