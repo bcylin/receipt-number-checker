1a30229214592efab899caa1e98c940697c6225f
Ben <b.cylin@gmail.com>
2012-11-22 16:18:32 +0800

    Remove fetching and parsing methods from drawModel

--- js/models/drawModel.js
+++ js/models/drawModel.js
@@ -14,13 +14,6 @@ define([
 
 return Backbone.Model.extend({
 
-	hasValue: false,
-
-	initialize: function() {
-		var url = this.get('url');
-		url && this.fetchData(url);
-	},
-
 	// Manually set prize and winning numbers
 	setData: function(data) {
 		var index = 0,
@@ -36,83 +29,13 @@ return Backbone.Model.extend({
 
 		this.clear({silent: true});
 		this.set({
-			months: data.months,
+			months: data.months.replace(/0(?=[1-9])/g, ""),
 			numberList: numberList,
 			prizeNameOfID: prizeNameOfID
 		});
 
 		this.sortDataByType();
 		this.createNumberNameMapping();
-	},
-
-	// Get raw HTML from the official website, extract winning numbers
-	fetchData: function(url) {
-		var self = this,
-			dfd = this.fetching = new $.Deferred();
-
-		var processData = function(content) {
-			self.rawdata = content;
-			self.extractDate(content);
-			self.extractNumbers(content);
-			self.sortDataByType();
-			self.createNumberNameMapping();
-			dfd.resolve();
-		};
-
-		// get the cached files
-		$.ajax({
-			url: 'cache/' + url.match(/[A-Z\_\d]+\.htm[l]?/ig)[0],
-			dataType: 'html',
-			success: function(content) {
-				processData(content);
-			},
-			error: function(content) {
-				console.log('Fail fetching URL: ' + url);
-				dfd.reject();
-			}
-		});
-
-		return dfd.promise();
-	},
-
-	// Helper function: extract date from HTML raw data
-	// @param {string} HTML rawdata
-	extractDate: function(rawdata) {
-		var caption = rawdata.match(/<div\ class\=\"caption\">.+月/ig),
-			date = caption || [""],
-			months = date[0].match(/[\d-\ ]+月/);
-		if (months) {
-			this.set('months', months[0]);
-		}
-	},
-
-	// Helper function: extract numbers from HTML raw data
-	// @param {string} HTML rawdata
-	extractNumbers: function(rawdata) {
-
-		var data = rawdata.replace(/<!DOCTYPE.+<\/head>|[\t\n]+|\ \ |<\/html>/g, ""),
-			$info = $(data).last();
-
-		var self = this,
-			prizeNameOfID = {},
-			numberList = {};
-
-		// extract each prize
-		$info.find('.number').each(function(index, item) {
-			var numbers = $(item).text() !== "" ? $(item).text().match(/(\d+)/ig) : [""];
-			var name = $(item).closest('td').prev('th').text();
-
-			// name of this prize id
-			prizeNameOfID[index + 'thPrize'] = name === "頭獎" ? "頭獎至六獎" : name;
-
-			// winning numbers of this prize id
-			numberList[index + 'thPrize'] = numbers;
-		});
-
-		this.set({
-			numberList: numberList,
-			prizeNameOfID: prizeNameOfID
-		});
 	},
 
 	// Helper function: sort numbers into 2 categories
